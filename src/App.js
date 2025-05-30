d2b8c7e360913ff9d5f54f18d28f0ef3e36eb2ad
WilliamChn <wwilliamchenn@gmail.com>
2025-05-29 23:01:50 -0400

    Set up the basic foundation of portfolio. Added nav bar to top of page and routing.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import headshot from './assets/headshot.png';
+import background from './assets/background.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <img src={background} alt="background" className="bg-img" />
+
+      <nav className="navbar">
+        <a href="/" className="nav-left">
+          <img src={headshot} alt="William Chen" className="nav-headshot" />
+          <span className="nav-name">WILLIAM CHEN</span>
         </a>
-      </header>
+        <div className="nav-right">
+          <a href="#about">ABOUT</a>
+          <a href="#projects">PROJECTS</a>
+          <a href="#resume">RESUME</a>
+          <a href="#contact">CONTACT</a>
+        </div>
+      </nav>
+
+      <div className="hero">
+        <h1>William Chen's Portfolio</h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+
