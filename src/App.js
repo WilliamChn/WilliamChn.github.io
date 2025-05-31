29d97933521516563749626bf9a3fb6f67c470d9
WilliamChn <wwilliamchenn@gmail.com>
2025-05-31 01:32:01 -0400

    Added intro bio to the home page along with transitions. Set up "See More" button placeholder.

--- src/App.js
+++ src/App.js
@@ -32,6 +32,13 @@ function App() {
 
       <div className="bio">
         <h1>Hello, I'm William Chen</h1>
+        <p>A frontend-focused software engineer passionate about building smooth, responsive digital experiences.</p>
+        <p>Whether it’s full-stack projects, embedded systems, or frontend development, I thrive at the intersection of code and creativity. I enjoy transforming complex ideas into intuitive, fun interfaces, and I’m always exploring new tools and technologies to improve my craft.</p>
+          <div className="see-more-wrapper">
+            <a href="#about" className="see-more">
+              See More About Me <span className="arrow">→</span>
+            </a>
+          </div>
       </div>
     </div>
   );
