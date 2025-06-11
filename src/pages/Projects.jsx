11e988a24623623aee23952439916f2a4ca97fdc
WilliamChn <wwilliamchenn@gmail.com>
2025-06-10 20:36:59 -0400

    All project placeholders made with routing set up.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,6 +5,9 @@ import "../styles/MobileView.css";
 import "../styles/SocialIcons.css";
 import "../styles/Projects.css";
 import SmartFanImg from "../assets/Smart_Fan.png";
+import WorkoutImg from "../assets/Workout_App.png";
+import AviationCrashImg from "../assets/Airplane_EDA.png"
+import PBEMImg from "../assets/PBEM.png"
 import { Link } from "react-router-dom";
 
 function Projects() {
@@ -29,9 +32,39 @@ function Projects() {
       </div>
       <div className="projects-container">
         <div className="project-card">
-          <img src={SmartFanImg} alt="Smart Fan Project" className="project-image" />
+          <img src={WorkoutImg} alt="Workout App Project" className="project-image" />
           <div className="project-content">
-            <h2>Smart Fan</h2>
+            <h2>Fitness App</h2>
+            <p>
+              A smart fan system built using Arduino that automatically adjusts fan speed based on
+              temperature input. Integrated with a display to show real-time sensor data.
+            </p>
+            <Link to="/projects/smart-fan">
+              <button className="case-study-button">Case Study</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="projects-container">
+        <div className="project-card">
+          <img src={AviationCrashImg} alt="Aviation Crash Analysis Project" className="project-image" />
+          <div className="project-content">
+            <h2>Plane Crash Data EDA</h2>
+            <p>
+              A smart fan system built using Arduino that automatically adjusts fan speed based on
+              temperature input. Integrated with a display to show real-time sensor data.
+            </p>
+            <Link to="/projects/smart-fan">
+              <button className="case-study-button">Case Study</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="projects-container">
+        <div className="project-card">
+          <img src={PBEMImg} alt="Personality Based Event Project" className="project-image" />
+          <div className="project-content">
+            <h2>Personality Based Event Matching</h2>
             <p>
               A smart fan system built using Arduino that automatically adjusts fan speed based on
               temperature input. Integrated with a display to show real-time sensor data.
